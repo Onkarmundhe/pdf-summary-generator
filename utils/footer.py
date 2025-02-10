f2be858f6a0fe37c61f4ec3cb783abd266c4ff87
Onkarmundhe <onkar.mundhe.21033@iitgoa.ac.in>
2025-02-10 23:25:32 +0530

    change in tokens

--- utils/footer.py
+++ utils/footer.py
@@ -5,9 +5,9 @@ def show_footer():
     st.markdown(
         """
         <div class="footer">
-            <p style='font-size: 1rem; color: #666;'>
+            <p style='font-size: rem; color: #666;'>
                 <br/>
-                <span style='font-size: 0.7rem;'>
+                <span style='font-size: 1 rem;'>
                     Built with Streamlit ❤️
                 </span>
             </p>
